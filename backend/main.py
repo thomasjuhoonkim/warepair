969936deea6fb31a0c52232549527d96ae814fd5
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-08-22 13:49:07 -0400

    7

--- backend/main.py
+++ backend/main.py
@@ -92,7 +92,7 @@ def get_db_values():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('DROP TABLE IF EXISTS users;')
-    cur.execute('CREATE TABLE users (user_id varchar (100) PRIMARY KEY NOT NULL,'
+    cur.execute('CREATE TABLE users user_id varchar (100) PRIMARY KEY NOT NULL,'
                 'first_name varchar (50) NOT NULL,'
                 'last_name varchar (50) NOT NULL,'
                 'email varchar (50) NOT NULL,'
